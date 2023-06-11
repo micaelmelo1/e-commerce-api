7afdebcb9e999d5b506f9374b79cabbae061915e
Micael Melo <micael.melo@cashway.io>
2023-06-11 14:40:39 -0300

    feat: :building_construction: Model to WishList

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,16 @@
 class User < ActiveRecord::Base
   include LikeSearchable
   include Paginatable
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   include DeviseTokenAuth::Concerns::User
+
+  has_many :wish_items
 
   validates :name, presence: true
   validates :profile, presence: true
-  
+
   enum profile: { admin: 0, client: 1 }
 end
