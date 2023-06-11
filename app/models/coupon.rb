a655730a3d2149e16b8ba27f3474cee2e4466630
Micael Melo <micael.melo@cashway.io>
2023-06-10 21:59:47 -0300

    restart project e-commerce

---
+++ app/models/coupon.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Coupon < ApplicationRecord
+  include LikeSearchable
+  include Paginatable
+
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: { case_sensitive: false }
+  validates :status, presence: true
+  validates :discount_value, presence: true, numericality: { greater_than: 0 }
+  validates :due_date, presence: true, future_date: true
+
+  enum status: { active: 1, inactive: 2 }
+end
