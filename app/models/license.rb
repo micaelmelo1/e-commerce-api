7b24aeb3d7021ad0c77cef4e240600e808716f55
Micael Melo <micael.melo@cashway.io>
2023-06-17 20:16:11 -0300

    ajust indentation model Licence

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class License < ApplicationRecord
   include Paginatable
   include LikeSearchable
-  
+
   belongs_to :game
 
   validates :key, presence: true, uniqueness: { case_sensitive: false, scope: :platform }
   validates :platform, presence: true
   validates :status, presence: true
 
-  enum platform: { steam: 1, battle_net: 2, origin: 3 } 
+  enum platform: { steam: 1, battle_net: 2, origin: 3 }
   enum status: { available: 1, in_use: 2, inactive: 3 }
 end
