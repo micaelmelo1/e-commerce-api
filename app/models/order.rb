525a0fc29363613c88656990ffe988eca62d20e8
Micael Melo <micael.melo@cashway.io>
2023-06-17 18:42:44 -0300

    identation model Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,19 +3,28 @@
 class Order < ApplicationRecord
   DAYS_TO_DUE = 7
 
-  belongs_to :user
   belongs_to :coupon, optional: true
-
+  belongs_to :user
+  
+  validates :installments, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :payment_type, presence: true
   validates :status, presence: true, on: :update
   validates :subtotal, presence: true, numericality: { greater_than: 0 }
   validates :total_amount, presence: true, numericality: { greater_than: 0 }
-  validates :payment_type, presence: true
-  validates :installments, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  enum status: { processing_order: 1, processing_error: 2, waiting_payment: 3,
-                 payment_accepted: 4, payment_denied: 5, finished: 6 }
-
-  enum payment_type: { credit_card: 1, billet: 2 }
+  enum payment_type: { 
+    credit_card: 1,
+    billet: 2
+  }
+  
+  enum status: {
+    processing_order: 1,
+    processing_error: 2,
+    waiting_payment: 3,
+    payment_accepted: 4,
+    payment_denied: 5,
+    finished: 6 
+  }
 
   before_validation :set_default_status, on: :create
 
