294d1ec8ff814874756945a68eabddf9faf1b4bf
Micael Melo <micael.melo@cashway.io>
2023-06-17 19:19:46 -0300

    model Address

---
+++ app/models/address.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Address
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :street
+  attribute :number
+  attribute :city
+  attribute :state
+  attribute :post_code
+
+  validates :street, :number, :city, :state, :post_code, presence: true
+end
