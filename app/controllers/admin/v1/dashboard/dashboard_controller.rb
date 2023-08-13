d6a0cc7be64c94f9123891d96e50f4687d173439
micaelmelo1 <micael.melo@cashway.io>
2023-08-13 20:07:28 -0300

    feat: :sparkles: Dashboard Admin system

---
+++ app/controllers/admin/v1/dashboard/dashboard_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Admin::V1::Dashboard
+  class DashboardController < Admin::V1::ApiController
+    def get_date(date)
+      Time.parse(params[date], "%Y-%m-%d")
+    rescue
+      nil
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
 
   namespace :admin, defaults: { format: :json } do
@@ -14,6 +18,12 @@ Rails.application.routes.draw do
       resources :products
       resources :system_requirements
       resources :users
+      
+      namespace :dashboard do
+        resources :sales_ranges, only: :index
+        resources :summaries, only: :index
+        resources :top_five_products, only: :index
+      end
     end
   end
 
@@ -25,6 +35,12 @@ Rails.application.routes.draw do
       post '/coupons/:coupon_code/validations', to: 'coupon_validations#create'
       resources :products, only: %i[index show]
       resources :wish_items, only: %i[index create destroy]
+    end
+  end
+
+  namespace :juno do
+    namespace :v1 do
+      resources :payment_confirmations, only: :create
     end
   end
 end
