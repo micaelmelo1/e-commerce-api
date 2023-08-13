d95920012c9ec4032a60330bb0e2848cf1718444
micaelmelo1 <micael.melo@cashway.io>
2023-08-13 20:06:49 -0300

    refactor: Edit Line Item model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,10 +4,10 @@ class LineItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :payed_price, presence: true, numericality: { greater_than: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :payed_price, presence: true, numericality: { greater_than: 0 }
 
   def total
-    payed_price * quantity
+    self.payed_price * self.quantity
   end
 end
